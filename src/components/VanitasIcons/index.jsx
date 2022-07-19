3250f945ac78c6fcdbc065b9b385d449e2757097
darkido <darkido256@gmail.com>
2022-07-19 18:59:44 +0430

    [fix]: change styles

---
+++ src/components/VanitasIcons/index.jsx
@@ -0,0 +1,10 @@
+import PenImg from 'src/assets/pen.png'
+import HourglassImg from 'src/assets/hourglass.png'
+
+export const InkPenIcon = () => {
+    return <img src={PenImg} width="25" />
+}
+
+export const HourglassIcon = () => {
+    return <img src={HourglassImg} width="10" style={{ margin: '5px' }} />
+}
--- src/pages/ContentPage/index.jsx
+++ src/pages/ContentPage/index.jsx
@@ -1,10 +1,14 @@
 import React, { useState, useRef } from 'react';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
+import { Divider, Avatar } from '@mui/material';
+import { FaTwitter } from 'react-icons/fa';
 import Annotation from 'react-image-annotation'
 import Layout from 'src/components/Layout';
 import { mockAnnotations } from './mockData'
 import VaniImg from 'src/assets/demo/vaniiveRepo5.jpg'
+import { InkPenIcon, HourglassIcon } from 'src/components/VanitasIcons';
+
 import './style.scss';
 import { useMediaQuery } from '@mui/material';
 // https://twitter.com/OneVnC/status/1534867819451629568
@@ -76,6 +80,7 @@ const ContentPage = () => {
                 annotations={imgAnno.annotations}
                 type={imgAnno?.type}
                 value={imgAnno.annotation}
+                highlightColor='#7adcef'
                 disableAnnotation
                 disableOverlay
               />
@@ -91,29 +96,87 @@ const ContentPage = () => {
             <div className='tab-panel-container'>
               <div hidden={tabVal !== 0}>
                 <div className="content-info">
-                  <span className="title">Description:</span>
-                  <span className="description">
-
-                    #ヴァニイベ レポ⑤
-                    <br />
-                    シナリオには「ここで花江さんに早口言葉を言っている振りをしていただくことは可能でしょうか?」としか書かなかったのですが、出来上がった台本では⬇️の早口言葉を実際に最大5回連続言う流れになっていて思わず笑ってしまいました
-                    チャレンジの結果はぜひその目でお確かめ下さい</span>
-
+                  <div className="title">
+                    <InkPenIcon/>
+                    Description:
+                  </div>
+                  <div className="description animated-border-quote">
+                    <blockquote>
+                      <span className="first-line">
+                        シナリオには「ここで花江さんに早口言葉を言っている振りをしていただくことは可能でしょうか?」としか書かなかったのですが、出来上がった台本では⬇️の早口言葉を実際に最大5回連続言う流れになっていて思わず笑ってしまいました
+                        チャレンジの結果はぜひその目でお確かめ下さい
+                      </span>
+                      <cite>
+                        <a href="https://twitter.com/jun_mdesu">
+                          - Jun Muchizuki
+                          <FaTwitter />
+                        </a>
+                      </cite>
+                    </blockquote>
+                  </div>
                 </div>
-                <div>
-                  <span>Source: <a href="https://twitter.com/jun_mdesu/status/1534847373515849728"> @jun_mdesu </a></span>
+                <Divider />
+                <div className="content-info">
+                  <span className="title">
+                    <HourglassIcon />
+                    tags:
+                  </span>
+                  <span className="description" >
+                    <i>
+                      Seiyuu_Event, Vaniive, VA_Events
+                    </i>
+                  </span>
+                  {/* <span>Source: <a href="https://twitter.com/jun_mdesu/status/1534847373515849728"> @jun_mdesu </a></span> */}
                 </div>
+
               </div>
 
               <div hidden={tabVal !== 1}>
                 <div className="content-info">
-                  <span className="title">Translation:</span>
-                  <span className="description">Jun-san wrote a tonge twister and asked Hanae-san (VA of Vanitas) to pretend reading the tongue twister and Hanae-san ended up saying it 5 times in row which made Jun-san laugh a lot. (˶◠....◠˶) Hanae-san has really cool and charming personality!</span>
+                  <div className="title">
+                    <HourglassIcon />
 
+                    Translation:</div>
+                  <div className="description">
+                    <span className="first-line">
+                      Jun-san wrote a tonge twister and asked Hanae-san (VA of Vanitas) to pretend reading the tongue twister and Hanae-san ended up saying it 5 times in row which made Jun-san laugh a lot. (˶◠....◠˶)
+                    </span>
+                  </div>
+                </div>
+                <div className="content-info">
+                  <div className="title">
+                    <InkPenIcon />
+
+                    TL Notes:</div>
+                  <div className="description">
+                    <span className="first-line">
+                      Hanae-san has really cool and charming personality!
+                    </span>
+                  </div>
+                </div>
+                <Divider />
+
+                <div className="content-info credit">
+                  <span className="role">
+                  Translated By:
+                  </span>
+                  <div className="avatar">
+                    <span>
+                      <Avatar
+                        alt="OneVnC"
+                        src="/broken-image.jpg"
+                      >
+                        O
+                      </Avatar>
+                    </span>
+                    <span>
+                      OneVnC
+                    </span>
+
+                  </div>
                 </div>
 
-                <span>Transalted By:</span>
-                <span>TL Notes:</span>
+
               </div>
 
             </div>
